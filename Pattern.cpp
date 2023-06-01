be514391735d7507e7feee22cd69b3bc4853883b
Priyansh Shrivastava <priyansh.shrivastava@ext.soprabanking.com>
2023-06-01 16:34:47 +0530

    Adds notes and pattern solution files

---
+++ Pattern.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+// Write a code to print the 1 2 3 4 sequnce 4 times in new line.
+// int main(){
+//     for(int i=0; i<4; i++){
+//         cout << 1 << " " << 2 << " " << 3 << " " << 4 << endl;
+//     }
+// }
+// int main(){
+//     for(int i=1; i<=4; i++){
+//         for (int j=1; j<=4; j++){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// Write a code to print 1 2 3 then 4 5 6 and 7 8 9 in the last row
+// int main(){
+//     for (int i=1; i<=3; i++){
+//         for (int j=1; j<=3; j++){
+//             cout << j+(3*i-3) << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// Write a code to print a Triangle of side 4 and 4 using *
+// int main(){
+//     int count=1;
+//     while(count<5){
+//         for(int i=1; i<=count; i++){
+//             cout << '*' << " ";
+//         }
+//         cout << endl;
+//         count++;
+//     }
+// }
+
+// Write a code to print a triangle with row numbers
+// int main(){
+//     for(int i=1; i<=4; i++){
+//         for(int j=1; j<=i; j++){
+//             cout << i << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// Write a code to print a triangle with increasing count
+// int main(){
+//     int count=1;
+//     for(int i=1; i<=4; i++){
+//         for(int j=1; j<=i; j++){
+//             cout << count << ' ';
+//             count++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// Write a code to print 1 2 3 3 4 5 5 6 7 in triangualar format
+// int main(){
+//     int count=1;
+//     for(int i=1; i<=4; i++){
+//         for(int j=1; j<=i; j++){
+//             cout << count << ' ';
+//             if(j==i){
+//                 continue;
+//             }
+//             count++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// Write a code to print AAA BBB CCC
+// int main(){
+//     char var='A';
+//     for (int i=1; i<=3; i++){
+//         for(int j=1; j<=3; j++){
+//             cout << char(var+(i-1)) << " "; 
+//         }
+//         cout << endl;
+//     }
+// }

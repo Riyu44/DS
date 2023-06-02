4e27e61973346218ce7bbb57de686a650d5ad23f
Priyansh Shrivastava <priyansh.shrivastava@ext.soprabanking.com>
2023-06-02 15:01:33 +0530

    completion of the bitwise and lecture 5 (yt playlist)

--- bitwise.cpp
+++ bitwise.cpp
@@ -27,7 +27,65 @@ using namespace std;
 //     cout << a << " " << b << endl;
 // }
 
+// Output sum of integers from 1 to n
+// int main(){
+//     int count=0, n;
+//     cin >> n;
+//     for(int i=1; i<=n; i++){
+//         count += i;
+//     }
+//     cout << count;
+// }
+
+// Fibbonaci Series
+// int main(){
+//     int n=10;
+//     int a=0, b=1;
+//     cout << a << endl << b << endl;
+//     for(int i=1; i<=n; i++){
+//         int c = a+b;
+//         cout << c <<endl;
+//         a=b;
+//         b=c;
+//     }
+// }
+
+// Tell if a number is prime (isPrime)
+// int main(){
+//     int n;
+//     cin >> n;
+//     for(int i=2; i<=n/2; i++){
+//         if(n%i == 0){
+//             cout << false;
+//             return false;
+//         }
+//     }
+//     cout << true;
+//     return true;
+
+// Given an integer n return the difference between the product and sum of its digit.
+// int main(){
+//     int n;
+//     cin >> n;
+//     int multiply=1, add=0;
+//     while(n>0){
+//         int a = n%10;
+//         multiply *= a;
+//         add += a;
+//         n /= 10;
+//     }
+//     cout <<  (multiply-add);
+// }
+
+// Return the number of one bits present in a number
 int main(){
-    int a= 3;
-    cout << (25 * (++a));
+    int n=11;
+    int count=0;
+    while(n!=0){
+        if(n&1){
+            count++;
+        }
+        n=n>>1;
+    }
+    cout << count;
 }

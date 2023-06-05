4901c72e0e4bffd3d8967cb5dc86ce69025a33c1
Priyansh Shrivastava <priyansh.shrivastava@ext.soprabanking.com>
2023-06-05 14:43:38 +0530

    Adds number_system lecture

---
+++ number_system.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// Write a code to convert decimal to binary
+// int main(){
+//     int n;
+//     cin >> n;
+//     string bits="";
+//     while(n!=0){
+//         int temp = n&1;
+//         bits = to_string(temp) + bits;
+//         n=n>>1;
+//     }
+//     cout << bits;
+// }
+
+// Print a binary form of a negative integer

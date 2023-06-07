9c548a930f2e3dca79c5eaa8975d157b7f705888
Priyansh Shrivastava <priyansh.shrivastava@ext.soprabanking.com>
2023-06-07 12:48:40 +0530

    Leetcode Array problems

---
+++ switch_case.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+// Find Unique Element - Very good question
+// int main(){
+//     int ans = 0;
+//     int arr[] = {1, 2, 3, 4, 3, 2, 1};
+//     for(int i=0; i<arr.size(); i++){
+//         ans = (ans ^ arr[i]);
+//     }
+//     cout << ans;
+// }
